39c1413a64c71b8750ce56ea8f628eff82b341f8
Nathan Harrenstein <nathan.harrenstein@gmail.com>
2017-06-13 16:52:57 -0700

    Remove template class.

--- starter-code/scripts/article.js
+++ starter-code/scripts/article.js
@@ -19,7 +19,7 @@ Article.prototype.toHtml = function() {
   However, in our modules.css stylesheet, we gave all elements
   with a class of template a display of none. Let's make
   sure we're not accidentally hiding our cloned article! */
-
+  $newArticle.removeClass('template');
   if (!this.publishedOn) $newArticle.addClass('draft');
   $newArticle.data('category', this.category);
 
