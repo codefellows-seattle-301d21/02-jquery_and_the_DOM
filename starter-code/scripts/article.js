bc3d8f0c7ad4c20cfaeedf8aed053fa8a86ce74a
Nick Asmussen <nick.asmussen1@gmail.com>
2017-06-13 17:03:33 -0700

    Removes a class and adds fills in template
    Removes template class fomr new article object so it doesnt have display equal none
    On the contrustor prototype method fills the DOM template with the object properties

--- starter-code/scripts/article.js
+++ starter-code/scripts/article.js
@@ -19,6 +19,7 @@ Article.prototype.toHtml = function() {
   However, in our modules.css stylesheet, we gave all elements
   with a class of template a display of none. Let's make
   sure we're not accidentally hiding our cloned article! */
+  $newArticle.removeClass('template');
 
   if (!this.publishedOn) $newArticle.addClass('draft');
   $newArticle.data('category', this.category);
@@ -34,6 +35,11 @@ Article.prototype.toHtml = function() {
     5. publication date. */
 
     $newArticle.find('.byline a').html(this.author);
+    $newArticle.find('.byline a').attr('href', this.authorUrl);
+    $newArticle.find('h1').html(this.title);
+    $newArticle.find('.article-body').html(this.body);
+    $newArticle.find('.byline time').attr('datetime', this.publishedOn);
+    $newArticle.find('.byline time').text(this.publishedOn);
 
   // Display the date as a relative number of 'days ago'
   $newArticle.find('time').html('about ' + parseInt((new Date() - new Date(this.publishedOn))/60/60/24/1000) + ' days ago');
