5e4b1f9dcd68bf5c32d715230c74d6b925296f82
Tim Turner <timmyturner@me.com>
2017-06-13 17:36:48 -0700

    DOM traversal
    Adding in the code to populate article properties
    into the DOM.

--- starter-code/scripts/article.js
+++ starter-code/scripts/article.js
@@ -35,7 +35,11 @@ Article.prototype.toHtml = function() {
     4. article body, and
     5. publication date. */
 
-    $newArticle.find('.byline a').html(this.author);
+  $newArticle.find('.byline a').html(this.author);
+  $newArticle.find('.byline a').attr('href', this.authorUrl);
+  $newArticle.find('header h1').html(this.title);
+  $newArticle.find('.article-body').html(this.body);
+  $newArticle.find('.byline time').attr('datetime', this.publishedOn);
 
   // Display the date as a relative number of 'days ago'
   $newArticle.find('time').html('about ' + parseInt((new Date() - new Date(this.publishedOn))/60/60/24/1000) + ' days ago');
